e305439bbd3bc1c961cf6abcb9c449359a10dd07
Sidney Meade <sidmeade@umich.edu>
2022-04-05 15:34:19 -0400

    Extracted openings from lichess database

--- extract_opening.py
+++ extract_opening.py
@@ -15,9 +15,10 @@ if __name__ == "__main__":
             curr_opening = re.findall(r'"([^"]*)"', line)[0]
             if curr_opening not in openings.keys():
                 openings[curr_opening] = []
-        if '1. ' in line:
-            curr_line = line
-            openings[curr_opening].append(line)
+        if '15. ' in line: # we only want to consider openings that are played out (what if a player quits after move 3?)
+            if 'eval' not in line: # We want to ignore lines which have eval added to pgn
+                curr_line = line
+                openings[curr_opening].append(line)
 
     # Remove openings with fewer than 5 entries:
     rem_openings = []
@@ -27,19 +28,37 @@ if __name__ == "__main__":
     for open_name in rem_openings:
         del openings[open_name]
 
-    # For the openings remaining, find the maximum string that all openings contain
+    # For the openings remaining, find the maximum line that all openings contain
     for name, lines in openings.items():
         lines.sort(key=len)
         max_str = lines[0]
         for line in lines:
             curr_str = ""
+            # print("max_str: " + max_str)
+            # print("line: " + line)
             for i in range(min(len(max_str), len(line))):
+                # print("len(max_str): " + str(len(max_str)))
+                # print("len(line): " + str(len(line)))
                 if line[i] != max_str[i]:
                     max_str = curr_str
+                    break
                 else:
                     curr_str += line[i]
         final_openings[name] = max_str
 
+    # Remove openings which have early multiple branches (branch before move 4)
+    rem_openings = []
+    for open_name, line in final_openings.items():
+        if '4.' not in line:
+            rem_openings.append(open_name)
+    for open_name in rem_openings:
+        del final_openings[open_name]
+
     # Print the final openings:
     for name, line in final_openings.items():
         print(name + ': ' + line)
+
+    # print("Example opening strings: Scotch opening")
+    # print("Scotch opening final: " + final_openings["Scotch Game #3"])
+    # for opening in (openings["Scotch Game #3"]):
+    #     print(opening[:20])
